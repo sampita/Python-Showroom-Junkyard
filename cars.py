5c5cafd6ec88247355a69a0b108fcc8462a61edc
Sam Pita <samanthakpita@gmail.com>
2020-01-15 09:47:21 -0600

    finished practice

--- cars.py
+++ cars.py
@@ -20,3 +20,24 @@ showroom.update(['Tesla Truck', 'Honda CRV'])
 # You've sold one of your cars. Remove it from the set with the discard() method.
 showroom.discard('Tesla Truck')
 print(showroom)
+
+
+
+
+#Acquiring more cars
+
+#Now create another set of cars in a variable junkyard. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the showroom set.
+junkyard = {'Jeep Renegade', 'Kia Soul', 'Ford Thunderbird', 'Toyota Corolla', 'Hyundai Sonata'}
+
+# Use the intersection method to see which cars exist in both the showroom and that junkyard.
+intersect_table = list(showroom & junkyard)
+
+print("intersect_table:", intersect_table)
+
+# Now you're ready to buy the cars in the junkyard. Use the union method to combine the junkyard into your showroom.
+union_set = showroom.union(junkyard)
+# print("union:", union_set)
+
+# Use the discard() method to remove any cars that you acquired from the junkyard that you do not want in your showroom.
+union_set.discard('Toyota Corolla')
+print("union:", union_set)
